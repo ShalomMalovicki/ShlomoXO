3d63853a283d80f5831e5f13f50b312d10fec049
shalom malovicki ASUS-13-FLIP <malovick@g.jct.ac.il>
2024-02-20 17:52:10 +0200

    Add project files.

---
+++ ShlomoXO/Converter/CharToBrushConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+
+namespace ShlomoXO
+{
+    public class CharToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return Brushes.Black;
+
+            char c = (char)value;
+           
+            if (c == 'x')
+                return Brushes.Red;
+            else if (c == 'o')
+                return Brushes.Green;
+            else
+                return Brushes.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ShlomoXO/ViewModel/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShlomoXO
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        #region Private Fields 
+        private string playerX;
+        private string playerO;
+        private char[] board = { 'x', ' ', 'o', ' ', ' ', ' ', 'x', ' ', ' ' };
+        #endregion Private Fields
+
+      
+        #region Properties   
+        public string PlayerX
+        {
+            get => playerX;
+            set
+            {
+                playerX = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PlayerX)));
+            }
+        }
+        public string PlayerO
+        {
+            get => playerO;
+            set
+            {
+                playerO = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PlayerO)));
+            }
+        }
+        public char[] Board
+        {
+            get => board;
+            set
+            {
+                board = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Board)));
+            }
+        }
+        #endregion Properties
+    }
+}
